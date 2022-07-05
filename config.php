610471ff8dec7f0afd9124399f8d44261fce2ebe
Daniel Paiva (W10/Quadra) <danielantunespaiva@gmail.com>
2022-07-05 18:00:37 -0300

    mod01 aula11

---
+++ config.php
@@ -0,0 +1,73 @@
+<?php
+
+//VARIÁVEIS DO SERVIDOR LOCAL
+$servidor = 'localhost';
+$banco = 'portalead';
+$usuario = 'root';
+$senha = '';
+
+/*
+//VARIÁVEIS DO SERVIDOR HOSPEDADO
+$servidor = 'xxx';
+$banco = 'xxx';
+$usuario = 'xxx';
+$senha = 'localhost';
+*/
+
+//url sistema
+//ao invés de digitar $url_sistema = 'http://localhost/dashboard/www/portal-ead/';, automatiza da seguinte maneira:
+$url_sistema = "http://$_SERVER[HTTP_HOST]/"; //se for servidor local, armazena localhost, do contrário, armazena http://hugocursos.com.br, se este for o domínio em que estão hospedados os arquivos
+$url = explode("//", $url_sistema);
+if($url[1] == 'localhost/'){
+	$url_sistema = "http://$_SERVER[HTTP_HOST]/loja/";
+}
+
+//VARIÁVEIS GLOBAIS
+$nome_loja = 'Lojinha do Daniel'; //esse é o nome padrão, depois o usuário pode mudar o nome do site
+$email_loja = 'danielantunespaiva@gmail.com';
+$tel_loja = '(15) 3333-3333';
+$whastsapp_loja = '(15) 99180-5895';
+$whastsapp_loja_link = '5515991805895';
+
+date_default_timezone_set('America/Sao_Paulo');
+
+try {
+    //fora a PDO, existem outros tipos de conexões com o banco de dados, como mysqli
+    $pdo = new PDO("mysql:dbname=$banco; host=$servidor", "$usuario", "$senha");
+} catch (Throwable $th) { //Exception $e não funciona para mim
+    echo 'Erro ao conectar ao banco de dados! <br><br>' . $th;
+}
+
+//VERIFICA SE JÁ HÁ UM USUÁRIO ADMINISTRADOR CRIADO
+/*
+$query = $pdo->query("SELECT * FROM config");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$total_reg = @count($res);
+
+if ($total_reg == 0) { //se a tabela config não tiver nenhum registro
+
+    $pdo->query("INSERT into config SET nome_sistema = '$nome_sistema', email_sistema = '$email_sistema', tel_sistema = '$tel_sistema', logo = 'logo.png', icone = 'favicon.ico', logo_rel = 'logo.jpg', itens_pag = '12'"); //na biblioteca dompdf, relatório em pdf não faz leitura de png, somente jpg, por isso a logo do relatório tem que ser em .jpg
+    //por padrão mostra 12 itens por página
+} else { //se a tabela config já tiver dados
+
+    //RECUPERA OS DADOS DA TABELA CONFIG
+
+    $nome_sistema = $res[0]['nome_sistema']; //o usuário pode ter alterado o nome do sistema no painel de controle para um diferente que o criado inicialmente, daí ele tem que puxar esse novo nome, o mesmo para telefone e email 
+    $email_sistema = $res[0]['email_sistema']; 
+    $cnpj_sistema = $res[0]['cnpj_sistema']; 
+    $tipo_chave_pix = $res[0]['tipo_chave_pix']; 
+    $chave_pix = $res[0]['chave_pix']; 
+    $facebook_sistema = $res[0]['facebook']; 
+    $instagram_sistema = $res[0]['instagram']; 
+    $youtube_sistema = $res[0]['youtube']; 
+    $itens_pag = $res[0]['itens_pag']; 
+    $video_sobre = $res[0]['video_sobre']; 
+    //$logo = $res[0]['logo']; //não precisa pois o nome nunca muda
+    //$icone = $res[0]['icone']; 
+    //$logo_rel = $res[0]['logo_rel']; 
+    //$qrcode_pix = $res[0]['qrcode_pix'];  //não precisa pois o nome nunca muda
+
+
+}
+*/
+?>
